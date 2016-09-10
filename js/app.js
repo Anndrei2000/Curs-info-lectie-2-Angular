072a0d476de69e5d4d6acdfa265ae378b8ff3f4e
Anndrei2000 <andrei.gheorghe2000@yahoo.com>
2016-09-10 12:41:27 +0300

    Mesajj

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var app  = angular.module("myApp",[]);
+app.controller("TestCtrl",["$scope",'$http', function($scope, $http){
+	$http.get("https://jsonplaceholder.typicode.com/users").then(function(response){
+		console.log(response.data);
+		$scope.data = response.data;
+       });	
+}]);
